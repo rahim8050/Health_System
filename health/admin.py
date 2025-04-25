f94ed261474ac7c05aff63afb4927a8cfd60e5f6
rahim8050 <rahimranxx8050@gmail.com>
2025-04-25 13:39:14 +0100

    category creation

--- health/admin.py
+++ health/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Category,Program
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug':('name',)}
+@admin.register(Program)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+    prepopulated_fields = {'slug':('name',)}
--- health/views.py
+++ health/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from health.models import Program, Category
+
 
 # Create your views here.
 def index(request):
     return render(request,'base.html')
+def program_list(request,category_slug=None):
+    category = None
+    programs = Program.objects.filter(available=True)
+    categories = Category.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category,slug=category_slug)
+        programs = programs.filter(category=category)
+
+    return render(request, 'program/programs/programs.html',{
+        'category': category,
+        'programs': programs,
+        'categories': categories,
+    })
