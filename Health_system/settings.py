f62208349c489554e31505fdbd6801847d8bace1
rahim8050 <rahimranxx8050@gmail.com>
2025-04-25 13:53:54 +0100

    static files

--- Health_system/settings.py
+++ Health_system/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,5 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+MEDIA_URL = 'media/'
+STATIC_ROOT = BASE_DIR/'staticfiles'
+MEDIA_ROOT = BASE_DIR/'mediafiles'
 
 AUTH_USER_MODEL = 'users.CustomUser'
--- Health_system/urls.py
+++ Health_system/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
+from Health_system import settings
 from health import views
 
 urlpatterns = [
@@ -28,4 +28,4 @@ urlpatterns = [
     path('users/', include("users.urls")),
 
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
