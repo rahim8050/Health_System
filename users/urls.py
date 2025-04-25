5ba7a00ee3cadd0ef2e2d865328bfd1442b13537
rahim8050 <rahimranxx8050@gmail.com>
2025-04-25 12:05:51 +0100

    users

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ path("login/",Login.as_view(),name="login"),
 path("register/",RegisterUser.as_view(),name="register"),
 path("logout/", Logout.as_view(), name="logout"),
 path('profile/',views.profile,name="profile"),
+
 ]
