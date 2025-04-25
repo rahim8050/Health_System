bb1daf339bdae5de890207d3badb01cc37407b01
rahim8050 <rahimranxx8050@gmail.com>
2025-04-25 13:00:19 +0100

    category migrations

--- health/models.py
+++ health/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    class Meta:
+        verbose_name_plural = 'health_programs'
+    def __str__(self):
+        return self.name
+
+class Program(models.Model):
+    category = models.ForeignKey(Category,related_name="programs", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='programs', blank=True, null=True)
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse("health:detail", kwargs={"id": self.id, "slug": self.slug})
